dee682c6edf2b7e7b0c615b4dc4cbcdffab7782b
HazelRaven <108705327+HazelRaven@users.noreply.github.com>
2022-09-05 14:55:02 -0700

    Update index.js

--- index.js
+++ index.js
@@ -55,8 +55,7 @@ function displayForecast(response) {
  <h5 class="Monday"> ${formatDay(forecastDay.dt)}</h5> 
   <p class="card-text"> ${Math.round(forecastDay.temp.min)} ${Math.round(
           forecastDay.temp.max
-        )}</p>
- <img
+        )}</p><img
 src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
 alt=""width="42"/>
  
